e7d7ab9ac97b441b8f207150703ce1135decc86b
Alejandro Osorio <alejandro.osorio@walmart.com>
2022-10-28 17:21:05 -0500

    Add initial styles

--- sick-fits/frontend/pages/_document.js
+++ sick-fits/frontend/pages/_document.js
@@ -3,12 +3,12 @@ import Document, { Html, NextScript, Main } from 'next/document';
 export default class MyDocument extends Document {
   render() {
     return (
-      <Html lang='en-US'>
+      <Html lang="en-US">
         <body>
           <Main />
           <NextScript />
         </body>
       </Html>
-    )
+    );
   }
 }
