8b0a8ca89d382d752a832de1cdb0961c37993a15
Alejandro Osorio <alejandro.osorio@walmart.com>
2022-11-01 20:03:17 -0500

    Create custom hook for Forms

---
+++ sick-fits/frontend/components/CreateProduct.js
@@ -0,0 +1,42 @@
+import useForm from '../lib/useForm';
+
+export default function CreateProduct() {
+  const { inputs, handleChange, resetForm, clearForm } = useForm({
+    name: 'Ale',
+    price: 100,
+  });
+
+  return (
+    <form>
+      <label htmlFor="name">
+        Name
+        <input
+          type="text"
+          id="name"
+          name="name"
+          placeholder="name"
+          value={inputs.name}
+          onChange={handleChange}
+        />
+      </label>
+      <label htmlFor="price">
+        Price
+        <input
+          type="number"
+          id="price"
+          name="price"
+          placeholder="price"
+          value={inputs.price}
+          onChange={handleChange}
+        />
+      </label>
+
+      <button type="button" onClick={resetForm}>
+        Reset form
+      </button>
+      <button type="button" onClick={clearForm}>
+        Clear form
+      </button>
+    </form>
+  );
+}
