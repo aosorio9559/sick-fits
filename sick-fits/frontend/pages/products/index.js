91ffc6e07d437de3f0dbe1991c74b4fbb75608ca
Alejandro Osorio <alejandro.osorio@walmart.com>
2022-11-05 13:37:26 -0500

    Add pagination dynamic routing

---
+++ sick-fits/frontend/pages/products/index.js
@@ -0,0 +1,16 @@
+import { useRouter } from 'next/router';
+import Pagination from '../../components/Pagination';
+import Products from '../../components/Products';
+
+export default function ProductsPage() {
+  const { query } = useRouter();
+  const page = parseInt(query.page, 10);
+
+  return (
+    <div>
+      <Pagination page={page || 1} />
+      <Products />
+      <Pagination page={page || 1} />
+    </div>
+  );
+}
