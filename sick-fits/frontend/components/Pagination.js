e5dbf43d71e15e30318ec5d46a7c0bb97c5d99d5
Alejandro Osorio <alejandro.osorio@walmart.com>
2022-11-21 19:18:21 -0500

    Add pagination count

--- sick-fits/frontend/components/Pagination.js
+++ sick-fits/frontend/components/Pagination.js
@@ -25,7 +25,9 @@ export default function Pagination({ page }) {
   return (
     <PaginationStyles>
       <Head>
-        <title>Sick Fits - Page {page} of __</title>
+        <title>
+          Sick Fits - Page {page} of {pageCount}
+        </title>
       </Head>
       <Link href={`/products/${page - 1}`}>
         {/* We had to used <a> to add aria-disabled attribute */}
