8221b18166e2ef00a5ae8ea98c69a558a5063f9b
Alejandro Osorio <alejandro.osorio@walmart.com>
2023-03-27 17:51:35 -0500

    60: Displaying All Orders

--- sick-fits/frontend/pages/order/[id].js
+++ sick-fits/frontend/pages/order/[id].js
@@ -40,39 +40,41 @@ export default function SingleOrderPage({ query }) {
 
   const { order } = data;
 
-  return <OrderStyles>
-    <Head>
-      <title>Sick fits - {order.id}</title>
-    </Head>
-    <p>
-      <span>Order Id:</span>
-      <span>{order.id} </span>
-    </p>
-    <p>
-      <span>Charge:</span>
-      <span>{order.charge}</span>
-    </p>
-    <p>
-      <span>Order Total</span>
-      <span>{formatMoney(order.total)}</span>
-    </p>
-    <p>
-      <span>Item Count</span>
-      <span>{order.items.length}</span>
-    </p>
-    <div className='items'>
-      {order.items.map(item => (
-        <div className='order-item' key={item.id}>
-          <img src={item.photo.image.publicUrlTransformed} alt={item.title} />
-          <div className='item-details'>
-            <h2>{item.name}</h2>
-            <p>Qty: {item.quantity}</p>
-            <p>Each: {formatMoney(item.price)}</p>
-            <p>Sub Total: {formatMoney(item.price * item.quantity)}</p>
-            <p>{item.description}</p>
+  return (
+    <OrderStyles>
+      <Head>
+        <title>Sick fits - {order.id}</title>
+      </Head>
+      <p>
+        <span>Order Id:</span>
+        <span>{order.id} </span>
+      </p>
+      <p>
+        <span>Charge:</span>
+        <span>{order.charge}</span>
+      </p>
+      <p>
+        <span>Order Total</span>
+        <span>{formatMoney(order.total)}</span>
+      </p>
+      <p>
+        <span>Item Count</span>
+        <span>{order.items.length}</span>
+      </p>
+      <div className='items'>
+        {order.items.map(item => (
+          <div className='order-item' key={item.id}>
+            <img src={item.photo.image.publicUrlTransformed} alt={item.title} />
+            <div className='item-details'>
+              <h2>{item.name}</h2>
+              <p>Qty: {item.quantity}</p>
+              <p>Each: {formatMoney(item.price)}</p>
+              <p>Sub Total: {formatMoney(item.price * item.quantity)}</p>
+              <p>{item.description}</p>
+            </div>
           </div>
-        </div>
-      ))}
-    </div>
-  </OrderStyles>;
+        ))}
+      </div>
+    </OrderStyles>
+  );
 }
--- sick-fits/frontend/pages/orders.js
+++ sick-fits/frontend/pages/orders.js
@@ -1,3 +1,104 @@
+import { useQuery } from '@apollo/client';
+import gql from 'graphql-tag';
+import ErrorMessage from '../components/ErrorMessage';
+import OrderStyles from '../components/styles/OrderStyles';
+import Head from 'next/head';
+import formatMoney from '../lib/formatMoney';
+import styled from 'styled-components';
+import OrderItemStyles from '../components/styles/OrderItemStyles';
+import Link from 'next/link';
+
+const USER_ORDERS_QUERY = gql`
+  query USER_ORDERS_QUERY {
+    allOrders {
+      id
+      charge
+      total
+      user {
+        id
+      }
+      items {
+        id
+        name
+        description
+        price
+        quantity
+        photo {
+          image {
+            publicUrlTransformed
+          }
+        }
+      }
+    }
+  }
+`;
+
+const OrderUl = styled.ul`
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(350px, 1fr));
+  grid-gap: 4rem;
+`;
+
+function pluralizeWord(word, quantity) {
+  return quantity === 1 ? word : `${word}s`;
+}
+
+function countItemsInAnOrder(order) {
+  return order.items.reduce((tally, item) => tally + item.quantity, 0);
+}
+
 export default function OrdersPage() {
-  return <p>orders page</p>;
+  const { data, error, loading } = useQuery(USER_ORDERS_QUERY);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <ErrorMessage error={error}></ErrorMessage>;
+
+  const { allOrders } = data;
+
+  return (
+    <div>
+      <Head>
+        <title>Your Orders ({allOrders.length})</title>
+      </Head>
+      <h2>You have {allOrders.length} orders!</h2>
+      <OrderUl>
+        {allOrders.map(order => {
+          const itemsInAnOrder = countItemsInAnOrder(order);
+          const productQuantity = order.items.length;
+
+          return (
+            <OrderItemStyles key={order.id}>
+              <Link href={`/order/${order.id}`}>
+                <a>
+                  <div className='order-meta'>
+                    <p>
+                      {itemsInAnOrder}
+                      {' '}
+                      {pluralizeWord('Item', itemsInAnOrder)}
+                    </p>
+                    <p>
+                      {productQuantity}
+                      {' '}
+                      {pluralizeWord('Product', productQuantity)}
+                    </p>
+                    <p>{formatMoney(order.total)}</p>
+                  </div>
+                  <div className="images">
+                    {order.items.map(item =>
+                      <img
+                        key={item.id}
+                        src={item.photo?.image?.publicUrlTransformed}
+                        alt={item.name}
+                      />
+                    )}
+                  </div>
+                </a>
+              </Link>
+            </OrderItemStyles>
+          );
+        })}
+      </OrderUl>
+    </div>
+
+  );
 }
