a543bf8f98dc252c2c2ab0b153db1d8620216943
Alejandro Osorio <alejandro.osorio@walmart.com>
2023-05-30 17:52:00 -0500

    64: Add Permissions Access Functions

--- sick-fits/backend/access.ts
+++ sick-fits/backend/access.ts
@@ -1,7 +1,22 @@
 // Simply put, access control will return a yes or no depending on the user's session
 
+import { permissionsList } from './schemas/fields';
 import { ListAccessArgs } from './types';
 
 export function isSignedIn({ session }: ListAccessArgs) {
   return !!session;
 }
+
+const generatedPermissions = Object.fromEntries(
+  permissionsList.map((permission) => [
+    permission,
+    function ({ session }: ListAccessArgs) {
+      return !!session?.data.role?.[permission];
+    },
+  ])
+);
+
+// Check if a user meets a criteria
+export const permissions = {
+  ...generatedPermissions,
+};
