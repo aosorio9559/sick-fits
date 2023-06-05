e58fc1d7116a7c17d438424b02e480205c3927d2
Alejandro Osorio <alejandro.osorio@walmart.com>
2023-06-05 17:26:18 -0500

    67: Add Cart and Order based Rules

--- sick-fits/backend/access.ts
+++ sick-fits/backend/access.ts
@@ -25,6 +25,9 @@ export const permissions = {
 // Rules can return a boolean or a filter which limits which products they can CRUD
 export const rules = {
   canManageProducts({ session }: ListAccessArgs) {
+    if (!isSignedIn({ session })) {
+      return false;
+    }
     // 1. Do they have the permission of canManageProducts?
     if (permissions.canManageProducts({ session })) {
       return true;
@@ -33,7 +36,34 @@ export const rules = {
     // This will bind as a WHERE clause in the graphQL API
     return { user: { id: session.itemId } };
   },
+  canOrder({ session }: ListAccessArgs) {
+    if (!isSignedIn({ session })) {
+      return false;
+    }
+    // 1. Do they have the permission of canManageCart?
+    if (permissions.canManageCart({ session })) {
+      return true;
+    }
+    // 2. If not, do they own this item?
+    // This will bind as a WHERE clause in the graphQL API
+    return { user: { id: session.itemId } };
+  },
+  canManageOrderItems({ session }: ListAccessArgs) {
+    if (!isSignedIn({ session })) {
+      return false;
+    }
+    // 1. Do they have the permission of canManageCart?
+    if (permissions.canManageCart({ session })) {
+      return true;
+    }
+    // 2. If not, do they own this item?
+    // This will bind as a WHERE clause in the graphQL API
+    return { order: { user: { id: session.itemId } } };
+  },
   canReadProducts({ session }: ListAccessArgs) {
+    if (!isSignedIn({ session })) {
+      return false;
+    }
     if (permissions.canManageProducts({ session })) {
       return true; // They can read all products
     }
